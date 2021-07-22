8af7aa529871f4510af189cfc782f3a107fd3565
eexileddYT <87630983+eexileddYT@users.noreply.github.com>
2021-07-22 16:11:46 +0800

    Add files via upload

---
+++ lesson4HW_1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 22 16:00:17 2021
+
+@author: 蔡侑廷
+
+"""
+
+
+for x in range(11):
+    print('*' * x)
---
+++ lesson4HW_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 22 15:58:51 2021
+
+@author: 蔡侑廷
+"""
+
+
+for x in range(10, 0, -1):
+    print('*' * x)
